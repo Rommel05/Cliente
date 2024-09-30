f076a83b178f0bac80e310ccaf56eeeb1021c76e
Rommel <rrommel5@gmail.com>
2024-09-30 18:17:27 +0200

    v4

--- JavaScript/Programación avanzada/DOM/Anexo/Ejercicio 1/script.js
+++ JavaScript/Programación avanzada/DOM/Anexo/Ejercicio 1/script.js
@@ -12,6 +12,15 @@ function totalEnlaces() {
         let contenido = document.createTextNode(enlaces.length);
         p.appendChild(contenido);
         document.body.appendChild(p);
+
+        let cerrar = document.createElement("button");
+        let buttonContent = document.createTextNode("Cerrar");
+        cerrar.appendChild(buttonContent);
+        document.body.appendChild(cerrar);
+        cerrar.addEventListener("click",function() {
+            document.body.removeChild(p);
+            document.body.removeChild(cerrar);
+        })
     });
 }
 
@@ -19,10 +28,10 @@ function totalEnlaces() {
 function refEnlaces() {
     let boton = document.getElementsByTagName("button")[1];
     boton.addEventListener("click", function() {
-        let tile = document.createElement("h3");
+        let title = document.createElement("h3");
         let content = document.createTextNode("Links")
-        tile.appendChild(content);
-        document.body.appendChild(tile);
+        title.appendChild(content);
+        document.body.appendChild(title);
 
         let ul = document.createElement("ul");
 
@@ -33,7 +42,7 @@ function refEnlaces() {
         for (let i = 0; i < links.length; i++) {
             let href = links[i].getAttribute("href");
             if (!uniqueLinks.includes(href)) {
-                uniqueLinks.push(href);
+                uniqueLinks.push(href)
                 let li = document.createElement("li");
                 let contenido = document.createTextNode(href);
                 li.appendChild(contenido);
@@ -41,6 +50,16 @@ function refEnlaces() {
             }
         }
         document.body.appendChild(ul);
+
+        let cerrar = document.createElement("button");
+        let buttonContent = document.createTextNode("Cerrar");
+        cerrar.appendChild(buttonContent);
+        document.body.appendChild(cerrar);
+        cerrar.addEventListener("click",function() {
+            document.body.removeChild(ul);
+            document.body.removeChild(title);
+            document.body.removeChild(cerrar);
+        })
         
     })
 }
@@ -49,16 +68,16 @@ function refEnlaces() {
 function refEnlacesPorParrafos() {
     let boton = document.getElementsByTagName("button")[2];
     boton.addEventListener("click", function() {
+        let contenedor = document.createElement("div");
+
         let p = document.getElementsByTagName("p");
-        let parrafos = p.length
-        for(let i = 0; i < parrafos; i++) {
+        let parrafos = p.length;
+        for (let i = 0; i < parrafos; i++) {
             let title = document.createElement("h3");
             let content = document.createTextNode("Párrafo " + (i + 1));
             title.appendChild(content);
-            document.body.appendChild(title);
-
+            contenedor.appendChild(title); 
             let ul = document.createElement("ul");
-
             let links = p[i].getElementsByTagName("a");
             for (let j = 0; j < links.length; j++) {
                 let li = document.createElement("li");
@@ -66,7 +85,17 @@ function refEnlacesPorParrafos() {
                 li.appendChild(contenido);
                 ul.appendChild(li);
             }
-            document.body.appendChild(ul);
+            contenedor.appendChild(ul); 
         }
-    })
+        document.body.appendChild(contenedor);
+
+        let cerrar = document.createElement("button");
+        let buttonContent = document.createTextNode("Cerrar");
+        cerrar.appendChild(buttonContent);
+        document.body.appendChild(cerrar);
+        cerrar.addEventListener("click", function() {
+            document.body.removeChild(contenedor);
+            document.body.removeChild(cerrar);
+        });
+    });
 }
