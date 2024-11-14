70babe5ffd2ce625aa6ea509bc5dce7a882123a2
Rommel <rrommel5@gmail.com>
2024-11-14 13:37:52 +0100

    BD AJAX

---
+++ Ajax/anexo/Ejercicio 1/connection.php
@@ -0,0 +1,30 @@
+<?php
+$opciones = array(
+    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_PERSISTENT => true
+);
+
+try {
+    $pdo = new PDO(
+        'mysql:host=localhost;dbname=libros;charset=utf8',
+        'root',
+        'sa',
+        $opciones
+    );
+} catch (PDOException $e) {
+    die("Error de conexión: " . $e->getMessage());
+};
+
+$libroId = $_GET['id'] ?? null;
+
+if ($libroId) {
+    $sql = "SELECT * FROM libro WHERE id = :id";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':id', $libroId, PDO::PARAM_INT);
+    $stmt->execute();
+
+    $libro = $stmt->fetch(PDO::FETCH_ASSOC);
+}
+
+?>
---
+++ Ajax/anexo/Ejercicio 1/script.js
@@ -0,0 +1,12 @@
+window.onload = () => {
+    
+}
+
+function showBook() {
+    let div = document.getElementById("contenedor");
+    
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET','connection.php')
+}
+
