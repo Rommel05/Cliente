26a38212216701b672c80456434b3cf4f1f974d3
Rommel <rrommel5@gmail.com>
2024-11-15 09:03:00 +0100

    Ejercicio 1

--- Ajax/anexo/Ejercicio 1/connection.php
+++ Ajax/anexo/Ejercicio 1/connection.php
@@ -25,6 +25,27 @@ if ($libroId) {
     $stmt->execute();
 
     $libro = $stmt->fetch(PDO::FETCH_ASSOC);
-}
+
+    if ($libro ) {
+        echo "<table border='1'>";
+        echo "<tr>";
+        echo "<th>ID</th>";
+        echo "<td>" . $libro['id'] . "</td>";
+        echo "<th>Título</th>";
+        echo "<td>" . $libro['titulo'] . "</td>";
+        echo "<th>Autor</th>";
+        echo "<td>" . $libro['autor'] . "</td>";
+        echo "<th>Año de Publicación</th>";
+        echo "<td>" . $libro['anio_publicacion'] . "</td>";
+        echo "<th>Genero</th>";
+        echo "<td>" . $libro['genero'] . "</td>";
+        echo "</tr>";
+        echo "</table>";
+    } else {
+        echo "Libro no encontrado.";
+    }
+} else {
+    echo "No se ha proporcionado un ID de libro.";
+};
 
 ?>
--- Ajax/anexo/Ejercicio 1/script.js
+++ Ajax/anexo/Ejercicio 1/script.js
@@ -1,12 +1,43 @@
 window.onload = () => {
-    
+    showBook();
 }
 
 function showBook() {
     let div = document.getElementById("contenedor");
-    
     const xhr = new XMLHttpRequest();
 
-    xhr.open('GET','connection.php')
+    let form = document.getElementById("form");
+    let inputs = form.getElementsByTagName("select");
+    
+    for (let i = 0; i < inputs.length; i++) {
+        inputs[i].addEventListener("change", () => {
+            let id = inputs[i].value;
+            div.innerHTML = "";
+            if (id == "") {
+                let p1 = document.createElement("p");
+                let content1 = document.createTextNode("");
+                p1.appendChild(content1);
+                div.appendChild(p1);
+            }
+            
+            xhr.open('GET','connection.php?id=' + id);
+            xhr.responseType = "text";
+
+            xhr.addEventListener("readystatechange", () => {
+                if (xhr.readyState == 4 && xhr.status == 200) {
+                    div.innerHTML = xhr.response
+                }
+            })
+
+            xhr.send();
+        })
+
+        
+    }
+    
+    
+    
+
+    
 }
 
